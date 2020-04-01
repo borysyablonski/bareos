6588308f7b0ad64d7729e994e1432def5830e1d0
Frank Ueberschar <frank.ueberschar@bareos.com>
2020-04-01 16:59:05 +0200

    libcloud-plugin: implement cancel and limit queue size for discovered objects

--- core/src/plugins/filed/BareosFdPluginBaseclass.py
+++ core/src/plugins/filed/BareosFdPluginBaseclass.py
@@ -211,6 +211,8 @@ class BareosFdPluginBaseclass(object):
                 context, 100, "handle_plugin_event called with bEventEndBackupJob\n"
             )
 
+            return self.end_backup_job(context)
+
         elif event == bEventType["bEventEndFileSet"]:
             bareosfd.DebugMessage(
                 context, 100, "handle_plugin_event called with bEventEndFileSet\n"
@@ -239,6 +241,13 @@ class BareosFdPluginBaseclass(object):
 
         return bRCs["bRC_OK"]
 
+    def end_backup_job(self, context):
+        """
+        End of Backup Job. 
+        Overload this to arrange whatever you have to do at this time.
+        """
+        return bRCs["bRC_OK"]
+    
     def start_backup_job(self, context):
         """
         Start of Backup Job. Called just before backup job really start.
